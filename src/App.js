57cc79dd1e3a88aebaa83052a507bc259d88c825
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2023-02-27 19:29:28 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState } from 'react'
+const apiHost = process.env.REACT_APP_API_HOST
+export default function App() {
+  const [now, setNow] = useState()
+  async function onClick() {
+    const res = await fetch(`${apiHost}/now`)
+    const json = await res.json()
+    setNow(json)
+  }
+  return <div>
+    <button onClick={onClick}>Present</button>
+    {now && <p>{now}</p>}
+  </div>
 }
-
-export default App;
